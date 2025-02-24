3690846c7241359b97ecf22ce720e2f0bb4debbd
AKSHAY R <akshay.renjith.1@gmail.com>
2025-02-24 15:28:28 +0530

    Updated Command Invoker
    - Created an Undo() method inside Command Invoker.
    - It removes the latest executed command from the command registry and call it's Undo() method.

--- Assets/Scripts/Commands/CommandInvoker.cs
+++ Assets/Scripts/Commands/CommandInvoker.cs
@@ -1,3 +1,4 @@
+using Command.Main;
 using System.Collections.Generic;
 
 namespace Command.Commands
@@ -14,6 +15,15 @@ namespace Command.Commands
         public void ExecuteCommand(ICommand commandToExecute) => commandToExecute.Execute();
 
         public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister);
+        public void Undo()
+        {
+            if (!RegistryEmpty() && CommandBelongsToActivePlayer())
+            {
+                commandRegistry.Pop().Undo();
+            }
+        }
+        private bool RegistryEmpty() => commandRegistry.Count == 0;
+        private bool CommandBelongsToActivePlayer() => (commandRegistry.Peek() as UnitCommand).commandData.ActorPlayerID == GameService.Instance.PlayerService.ActivePlayerID;
     }
 
 }
